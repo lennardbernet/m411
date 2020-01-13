2d690ab32d2012a60594a92ad3573fb13a698500
lennardbernet <43334515+lennardbernet@users.noreply.github.com>
2020-01-13 16:16:18 +0100

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,5 +1,3 @@
-package m411.algorithmus;
-
 import java.util.Scanner;
 
 public class Main {
@@ -8,29 +6,48 @@ public class Main {
 
 		Scanner input = new Scanner(System.in);
 		System.out.println("String 1");
-		String string11 = input.nextLine();
-		String[] string1 = string11.split("");
+		String stringGanz1 = input.nextLine();
+		String[] string1 = stringGanz1.split("");
 		System.out.println("String 2");
-		String string22 = input.nextLine();
-		String[] string2 = string22.split("");
+		String stringGanz2 = input.nextLine();
+		String[] string2 = stringGanz2.split("");
 
+		// i = suchposition im String 1
 		for (int i = 0; i < string1.length - (string2.length - 1); i++) {
-			int found = 0;
-			for (int j = 1; j <= string2.length; j++) {
-				if (string1[i + string2.length - j].equals(string2[string2.length - j])) {
-					found += 1;
+			Boolean contains = false;
+			// Kommt kein Char vom String 2 im String 1 vor wird die Suchposition um die
+			// lÃ¤nge des String 2 nach vorne geschoben
+			if (stringGanz1.length() > i + 2) {
+				for (int j = 0; j < string2.length; j++) {
+					if (stringGanz2.contains(string1[i + j])) {
+						contains = true;
+					}
 				}
 			}
 
-			if (found == string2.length) {
-				System.out.println("string 2 " + string22 + " found on string 1 position " + (i + 1));
+			// Ist ein einzelner Char von String 2 im String 1 vorhanden wird hochgezÃ¤hlt
+			// bis die lÃ¤nge des String 2
+			int found = 0;
+			if (contains == true || stringGanz1.length() <= i + 2) {
+				for (int j = 1; j <= string2.length; j++) {
+					if (string1[i + string2.length - j].equals(string2[string2.length - j])) {
+						found += 1;
+					}
+				}
 			}
 
-			if (found == 0) {
+			// wurde eine Ãœbereinstimmung der zwei String gefunden wird die Startposition
+			// ausgegeben
+			if (found == string2.length) {
+				System.out.println("String 2 " + stringGanz2 + " found on String 1 position " + (i + 1));
+			}
+
+			// nach vorne schieben der nÃ¤chsten Suchposition
+			if (contains == false) {
 				i += string2.length - 1;
 			}
-
 		}
 
+		input.close();
 	}
 }
